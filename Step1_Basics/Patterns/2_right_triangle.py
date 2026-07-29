7d64a1c2201f1bcc8c98f733324dc91de84493c7
Its-Nes <itsnes4@gmail.com>
2026-07-29 16:58:11 +0530

    Right angled triangle code

---
+++ Step1_Basics/Patterns/2_right_triangle.py
@@ -0,0 +1,24 @@
+# Pattern 2 - Right-Angled Triangle
+
+"""
+Problem:
+Given an integer n, print a right-angled triangle
+consisting of '*' characters.
+"""
+
+def triangle(n):
+    for i in range(1,n+1):
+        for j in range(i):
+            print("*", end=" ")
+        print()
+    print()         # blank line after each test case
+
+# Driver code
+t=int(input("No. of testcases: "))      # 3
+n=list(map(int, input().split()))       # 1 2 3
+for i in range(t):
+    triangle(n[i])
+
+
+# t.c: O(n^2) per test case
+# s.c: O(1)
