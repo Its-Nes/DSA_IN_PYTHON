c2c68b0a28cda007288e709dbb678e56e8a54ad7
Its-Nes <itsnes4@gmail.com>
2026-07-30 09:07:26 +0530

    pyramid code

--- Step1_Basics/Patterns/6_pyramid.py
+++ Step1_Basics/Patterns/6_pyramid.py
@@ -1,13 +1,33 @@
-def triangle7(n):
+# Pattern 6 - Pyramid Pattern
+
+"""
+Problem:
+Given an integer n, print a pyramid pattern
+consisting of '*' characters.
+
+Example:
+Input:
+n = 4
+
+Output:
+      *
+    * * *
+  * * * * *
+* * * * * * *
+"""
+
+def pyramid(n):
     for i in range(n):
-        print("  "*(n-i)+"* "*(2*i+1))
+        print("  " * (n - i - 1) + "* " * (2 * i + 1))
+    print()
 
-triangle7(4)
 
-"""
-4
-        * 
-      * * * 
-    * * * * *
-  * * * * * * *
-"""
+
+# Driver Code
+t = int(input("No. of testcases: "))
+nums = list(map(int, input().split()))
+for i in range(t):
+    pyramid(nums[i])
+
+# T.C: O(n²)
+# S.C: O(1)
